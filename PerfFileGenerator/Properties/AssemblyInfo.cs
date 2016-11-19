5764fd0479c3b9e74a07d0b0fffc292bfc37f6be
Douglas Spangenberg <aus_doug@hotmail.com>
2016-11-19 12:02:29 +1030

    Finalised 1.1

--- PerfFileGenerator/Properties/AssemblyInfo.cs
+++ PerfFileGenerator/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Performance File Generator")]
 [assembly: AssemblyDescription("Creates Grand Prix 4 Performance files from CSV input.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Performance File Generator")]
+[assembly: AssemblyCompany("AUS_doug")]
+[assembly: AssemblyProduct("GP4 Perf Gen")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
